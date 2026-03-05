5e7d524cff7a9a92dea83b73ec049933a6cfa50c
Daniel Jesus <dev.danieljs@gmail.com>
2026-03-04 21:42:28 -0300

    feat(debug): add terminal map renderer for movement visualization

--- include/player.h
+++ include/player.h
@@ -1,4 +1,4 @@
-/* ************************************************************************** */
+/******************************************************************************/
 /*                                                                            */
 /*                                                        :::      ::::::::   */
 /*   player.h                                           :+:      :+:    :+:   */
@@ -6,15 +6,34 @@
 /*   By: dajesus- <dajesus-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/03 17:10:00 by dajesus-          #+#    #+#             */
-/*   Updated: 2026/03/03 18:13:07 by dajesus-         ###   ########.fr       */
+/*   Updated: 2026/03/04 21:33:11 by dajesus-         ###   ########.fr       */
 /*                                                                            */
-/* ************************************************************************** */
+/******************************************************************************/
 
 #ifndef PLAYER_H
 # define PLAYER_H
 
 # include "structs.h"
 
-int	player_init(t_app *app);
+# define PLAYER_MOVE_SPEED 3.0
+# define PLAYER_MAX_DT 0.05
+# define PLAYER_COLLISION_RADIUS 0.2
+
+# define PLAYER_DEFAULT_DT 0.0166666666666667 // (1.0 / 60.0)
+
+# define USEC_PER_SEC 1000000L
+# define USEC_PER_SEC_DBL 1000000.0
+
+# define PLAYER_TERM_DEBUG 1
+
+# define TERM_RESET "\033[0m"
+# define TERM_WALL "\033[44m"
+# define TERM_FLOOR "\033[42m"
+# define TERM_VOID "\033[40m"
+# define TERM_PLAYER "\033[43m"
+
+int		player_init(t_app *app);
+void	player_update(t_app *app);
+void	player_debug_term(t_app *app);
 
 #endif
