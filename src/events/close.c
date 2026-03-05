7d6fc30f9b70f1cbe6ab8cc23352f27572d58597
Daniel Jesus <dev.danieljs@gmail.com>
2026-03-04 21:41:15 -0300

    refactor(events): keep close handler isolated

--- src/events/close.c
+++ src/events/close.c
@@ -1,4 +1,4 @@
-/* ************************************************************************** */
+/******************************************************************************/
 /*                                                                            */
 /*                                                        :::      ::::::::   */
 /*   close.c                                            :+:      :+:    :+:   */
@@ -6,15 +6,11 @@
 /*   By: dajesus- <dajesus-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/25 22:20:24 by dajesus-          #+#    #+#             */
-/*   Updated: 2026/02/25 23:43:35 by dajesus-         ###   ########.fr       */
+/*   Updated: 2026/03/04 21:33:55 by dajesus-         ###   ########.fr       */
 /*                                                                            */
-/* ************************************************************************** */
+/******************************************************************************/
 
 #include "../../include/cub3d.h"
-
-#ifndef XK_ESCAPE
-# define XK_ESCAPE 65307
-#endif
 
 int	on_destroy(void *param)
 {
@@ -25,17 +21,5 @@ int	on_destroy(void *param)
 		return (0);
 	app->running = 0;
 	mlx_loop_end(app->mlx.ptr);
-	return (0);
-}
-
-int	on_keydown(int keycode, void *param)
-{
-	t_app	*app;
-
-	app = (t_app *)param;
-	if (!app)
-		return (0);
-	if (keycode == XK_ESCAPE)
-		return (on_destroy(param));
 	return (0);
 }
