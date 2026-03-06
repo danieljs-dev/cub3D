4d00819965a9c6a124b96d13d388e9b65448b141
Vinicius Lima <vinislima@gmail.com>
2026-03-05 23:25:48 -0300

    feat: implement rotation with arrow keys

--- include/events.h
+++ include/events.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   events.h                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dajesus- <dajesus-@student.42.fr>          +#+  +:+       +#+        */
+/*   By: vinda-si <vinda-si@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/25 22:17:26 by dajesus-          #+#    #+#             */
-/*   Updated: 2026/02/25 23:42:29 by dajesus-         ###   ########.fr       */
+/*   Updated: 2026/03/05 23:00:03 by vinda-si         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,20 @@
 # define EVENTS_H
 
 # include "structs.h"
+
+#ifndef XK_ESCAPE
+# define XK_ESCAPE 65307
+#endif
+
+#ifndef XK_LEFT
+# define XK_LEFT 65361
+#endif
+
+#ifndef XK_RIGHT
+# define XK_RIGHT 65363
+#endif
+
+# define ROT_SPEED 0.05
 
 int	on_keydown(int keycode, void *param);
 int	on_keyup(int keycode, void *param);
--- src/events/close.c
+++ src/events/close.c
@@ -3,14 +3,15 @@
 /*                                                        :::      ::::::::   */
 /*   close.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dajesus- <dajesus-@student.42.fr>          +#+  +:+       +#+        */
+/*   By: vinda-si <vinda-si@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/25 22:20:24 by dajesus-          #+#    #+#             */
-/*   Updated: 2026/03/04 21:46:25 by dajesus-         ###   ########.fr       */
+/*   Updated: 2026/03/05 23:20:02 by vinda-si         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/cub3d.h"
+#include <stdio.h>
 
 int	on_destroy(void *param)
 {
@@ -21,5 +22,29 @@ int	on_destroy(void *param)
 		return (0);
 	app->running = 0;
 	mlx_loop_end(app->mlx.ptr);
+	return (0);
+}
+static void	rotate_player(t_player *p, double angle)
+{
+	double	old_dir_x;
+
+	old_dir_x = p->dir_x;
+	p->dir_x = p->dir_x * cos(angle) - p->dir_y * sin(angle);
+	p->dir_y = old_dir_x * sin(angle) + p->dir_y * cos(angle);
+}
+
+int	on_keydown(int keycode, void *param)
+{
+	t_app	*app;
+
+	app = (t_app *)param;
+	if (!app)
+		return (0);
+	if (keycode == XK_ESCAPE)
+		return (on_destroy(param));
+	if (keycode == XK_LEFT)
+		rotate_player(&app->player, -ROT_SPEED);
+	if (keycode == XK_RIGHT)
+		rotate_player(&app->player, ROT_SPEED);
 	return (0);
 }
