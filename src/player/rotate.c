dc26dd0522076b8d2184a21a1df1bc19cf1f7663
Daniel Jesus <dev.danieljs@gmail.com>
2026-03-06 22:58:29 -0300

    feat(player-rotate): init plane from spwan and rotate dir and plane at same time

--- src/player/rotate.c
+++ src/player/rotate.c
@@ -1,4 +1,4 @@
-/* ************************************************************************** */
+/******************************************************************************/
 /*                                                                            */
 /*                                                        :::      ::::::::   */
 /*   rotate.c                                           :+:      :+:    :+:   */
@@ -6,19 +6,23 @@
 /*   By: dajesus- <dajesus-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/06 01:46:58 by dajesus-          #+#    #+#             */
-/*   Updated: 2026/03/06 02:43:27 by dajesus-         ###   ########.fr       */
+/*   Updated: 2026/03/06 22:49:15 by dajesus-         ###   ########.fr       */
 /*                                                                            */
-/* ************************************************************************** */
+/******************************************************************************/
 
 #include "cub3d.h"
 
 void	rotate_player(t_player *p, double angle)
 {
 	double	old_dir_x;
+	double	old_plane_x;
 
 	old_dir_x = p->dir_x;
 	p->dir_x = p->dir_x * cos(angle) - p->dir_y * sin(angle);
 	p->dir_y = old_dir_x * sin(angle) + p->dir_y * cos(angle);
+	old_plane_x = p->plane_x;
+	p->plane_x = p->plane_x * cos(angle) - p->plane_y * sin(angle);
+	p->plane_y = old_plane_x * sin(angle) + p->plane_y * cos(angle);
 }
 
 void	player_rotate_update(t_app *app, double dt)
