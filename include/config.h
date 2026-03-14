514ca46e18644bcefcf8a85fbe43630afe9c36d5
Poukk <alexandrefilho.06@hotmail.com>
2026-03-14 16:38:21 -0300

    refactor: remove comments

--- include/config.h
+++ include/config.h
@@ -13,12 +13,10 @@
 #ifndef CONFIG_H
 # define CONFIG_H
 
-/* ---- window ---- */
 # define CUB3D_WIN_TITLE "cub3D"
 # define CUB3D_WIN_W 800
 # define CUB3D_WIN_H 600
 
-/* ---- key codes ---- */
 # define XK_ESCAPE 65307
 # define XK_W 119
 # define XK_A 97
@@ -27,7 +25,6 @@
 # define XK_LEFT 65361
 # define XK_RIGHT 65363
 
-/* ---- player ---- */
 # define PLAYER_MOVE_SPEED 3.0
 # define PLAYER_MAX_DT 0.05
 # define PLAYER_COLLISION_RADIUS 0.2
@@ -35,17 +32,14 @@
 # define CAMERA_PLANE_LEN 0.66
 # define PLAYER_DEFAULT_DT 0.0166666666666667
 
-/* ---- raycasting ---- */
 # define RAYCORE_CAM_SCALE 2.0
 # define RAYCORE_CAM_SHIFT 1.0
 # define RAY_HUGE 1e30
 
-/* ---- timing / fps ---- */
 # define USEC_PER_SEC 1000000L
 # define USEC_PER_SEC_DBL 1000000.0
 # define FPS_UPDATE_US 500000L
 
-/* ---- debug ---- */
 # define PLAYER_TERM_DEBUG 1
 # define FPS_DISPLAY 1
 
